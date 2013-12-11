759f0c5bed0defc6dd0df7fe76293968fba81b75
Dave J <contact_davej@ymail.com>
2013-12-11 23:31:47 +0100

    KeyLoaderFactory added.
    Should work with direct key and from local file.

---
+++ RiotGamesAPIWrapper/HechTex.RiotGamesAPIWrapper/KeyLoader/KeyLoaderFactory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace HechTex.RiotGamesAPIWrapper.KeyLoader
+{
+    /// <summary>
+    /// Factory class to get the API-Key from an input.
+    /// <para />
+    /// Discussable, whether this has to be a 'factory'.
+    /// </summary>
+    internal class KeyLoaderFactory
+    {
+        // TODO VERIFY!
+        private readonly static Regex API_PATTERN = new Regex(@"[\da-z]{8}(?:-[\da-z]{4}){3}[\da-z]{12}",
+                                                            RegexOptions.Compiled);
+        /// <summary>
+        /// Filters the key from the input. This might
+        /// be the key itself or a filename.
+        /// </summary>
+        /// <param name="input">The input string.</param>
+        /// <returns>The key as a string.</returns>
+        internal static string GetKey(string input)
+        {
+            if (API_PATTERN.IsMatch(input))
+                return FromKey(input);
+            else
+            {
+                // TODO already throw Exception if input no
+                //      key and no uri!
+                Uri u = new Uri(input);
+                // TODO if (!u.IsAbsoluteUri) throw Exception("NOPE");
+                if (u.IsFile)
+                    return FromLocalFile(input);
+                else
+                    return FromRemote(u);
+            }
+        }
+
+        private static string FromKey(string input)
+        {
+            return API_PATTERN.Match(input).Value;
+        }
+
+        private static string FromLocalFile(string input)
+        {
+            foreach (string line in File.ReadLines(input))
+                if (API_PATTERN.IsMatch(line))
+                    return FromKey(line);
+            throw new KeyLoadingException("Invalid key-file. No API-Key found!",
+                            input);
+        }
+
+        private static string FromRemote(Uri input)
+        {
+            throw new NotImplementedException("");
+        }
+    }
+
+    public class KeyLoadingException : Exception
+    {
+        /// <summary>
+        /// Input, got from caller.
+        /// </summary>
+        public string InputLine { get; private set; }
+
+        public KeyLoadingException(string exceptionstring, string input)
+            : base(exceptionstring)
+        {
+            InputLine = input;
+        }
+    }
+}
