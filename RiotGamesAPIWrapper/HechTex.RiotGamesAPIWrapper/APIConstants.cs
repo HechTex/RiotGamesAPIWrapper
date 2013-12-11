5d94f49256b55fe95af7ff902ca484ab9de60036
Florian Wilkens <floya@live.de>
2013-12-11 22:03:17 +0100

    Added APIConstants.cs containing enums describing API constants and main API class RiotGamesAPI

---
+++ RiotGamesAPIWrapper/HechTex.RiotGamesAPIWrapper/APIConstants.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace HechTex.RiotGamesAPIWrapper.APIConstants
+{
+    public enum MatchmakingQueues
+    {
+        Normal5v5Blind = 2
+    }
+
+    public enum Maps
+    {
+        SR              = 1,
+        SR_Autumn       = 2,
+        ProvingGrounds  = 3,
+        TT_original     = 4,
+        CrystalScar     = 8,
+        TT              = 10,
+        HowlingAbyss    = 12
+    }
+
+    public enum GameTypes
+    {
+        CUSTOM_GAME = 0,
+        ODIN        = 1,
+        ARAM        = 2,
+        TURORIAL    = 3
+    }
+}
---
+++ RiotGamesAPIWrapper/HechTex.RiotGamesAPIWrapper/RiotGamesAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HechTex.RiotGamesAPIWrapper
+{
+    public class RiotGamesAPI
+    {
+        private const string API_BASE_URL = @"http://prod.api.pvp.net/";
+    }
+}
