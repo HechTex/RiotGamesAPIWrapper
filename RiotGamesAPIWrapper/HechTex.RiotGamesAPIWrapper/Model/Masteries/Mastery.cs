088aac8706eae6307bfd0d4ac17b0ce6d11c7804
Dave J <contact_davej@ymail.com>
2013-12-14 13:08:45 +0100

    Removed *Page_s_ and added comments.

--- RiotGamesAPIWrapper/HechTex.RiotGamesAPIWrapper/Model/Masteries/Mastery.cs
+++ RiotGamesAPIWrapper/HechTex.RiotGamesAPIWrapper/Model/Masteries/Mastery.cs
@@ -11,8 +11,19 @@ namespace HechTex.RiotGamesAPIWrapper.Model.Masteries
         /* the actual "class" the API mentions is called "Talent"
          * but this should not be an issue, since its just JSON */
 
+        /// <summary>
+        /// Mastery/Talent id.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Mastery/Talent name.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Mastery/Talent rank.
+        /// </summary>
         public int Rank { get; set; }
     }
 }
--- RiotGamesAPIWrapper/HechTex.RiotGamesAPIWrapper/Model/Runes/RuneSlot.cs
+++ RiotGamesAPIWrapper/HechTex.RiotGamesAPIWrapper/Model/Runes/RuneSlot.cs
@@ -8,7 +8,14 @@ namespace HechTex.RiotGamesAPIWrapper.Model.Runes
 {
     public class RuneSlot
     {
+        /// <summary>
+        /// Rune slot id.
+        /// </summary>
         public int RuneSlotId { get; set; }
+
+        /// <summary>
+        /// Rune associated with the rune slot.
+        /// </summary>
         public Rune Rune { get; set; }
     }
 }
